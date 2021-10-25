64467142f2171c912f209aa596a759167b019f88
Alhassan <alhassan@walulel.com>
2021-10-25 16:08:43 +0000

    update readme, add logout functionality, add icon color

--- README.md
+++ README.md
@@ -7,9 +7,10 @@ Emplozongo project running on Laravel version 8.x
 1. Clone the project from [github](https://github.com/yaqoubhassan/employzongo.git) using the **HTTPS** option
 2. Configure your `.env` file 
 3. Install project dependencies `composer install` and `npm install`
-4. Run `composer require laravel/ui`
-5. Run `php artisan ui vue --auth`
-6. Run migrations and seeders `php artisan migrate --seed`
+4. Run `php artisan key:generate`
+5. Run `composer require laravel/ui`
+6. Run `php artisan ui vue --auth`
+7. Run migrations and seeders `php artisan migrate --seed`
 
 
 ## Third Party Packages
--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -93,7 +93,7 @@ scratch. This page gets rid of all links and provides the needed markup only.
         <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
             <li class="nav-item">
                 <router-link to="/dashboard" class="nav-link">
-                    <i class="nav-icon fas fa-tachometer-alt"></i>
+                    <i class="nav-icon fas fa-tachometer-alt blue"></i>
                     <p>
                     Dashboard
                     </p>
@@ -102,10 +102,10 @@ scratch. This page gets rid of all links and provides the needed markup only.
             
             <li class="nav-item">
                 <a href="#" class="nav-link">
-                <i class="nav-icon fas fa-cog"></i>
+                <i class="nav-icon fas fa-cog green"></i>
                 <p>
                     Management
-                    <i class="right fas fa-angle-left"></i>
+                    <i class="right fas fa-angle-left green"></i>
                 </p>
                 </a>
                 <ul class="nav nav-treeview">
@@ -126,7 +126,7 @@ scratch. This page gets rid of all links and provides the needed markup only.
 
             <li class="nav-item">
                 <router-link to="/profile" class="nav-link">
-                    <i class="nav-icon fas fa-user"></i>
+                    <i class="nav-icon fas fa-user orange"></i>
                     <p>
                     Profile
                     </p>
@@ -134,12 +134,19 @@ scratch. This page gets rid of all links and provides the needed markup only.
             </li>
 
             <li class="nav-item">
-                <a href="#" class="nav-link">
-                    <i class="nav-icon fas fa-power-off"></i>
-                    <p>
-                    Logout
-                    </p>
-                </a>
+              <a class="nav-link" href="{{ route('logout') }}"
+                onclick="event.preventDefault();
+                document.getElementById('logout-form').submit();">
+                <i class="nav-icon fas fa-power-off red"></i>
+                <p>
+                  {{ __('Logout') }}
+                </p>
+                
+              </a>
+
+              <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                  @csrf
+              </form>
             </li>
           
         </ul>
